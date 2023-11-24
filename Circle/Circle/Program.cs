ed8609123e0518612f2b669a22809414c2a29afa
Damian <sasakihaise1021@gmail.com>
2023-11-24 11:21:42 -0500

    Exercise rectangle and circle with implicit variable

---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,12 @@
+﻿var radius = 0d;
+var result = 0d;
+const double Pi = 3.14;
+Console.WriteLine("Welcome, let's calculate circles");
+
+Console.WriteLine("Enter the radio value: ");
+
+radius = Convert.ToDouble(Console.ReadLine());
+
+result = Pi * (radius * radius);
+
+Console.WriteLine("The area of circle is: " + result);
---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿var A = 0d;
+var B = 0d;
+var result = 0d;
+
+Console.WriteLine("Calculate the area of a rectangle");
+
+Console.WriteLine("\nEnter the value of 'A': ");
+A = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the value of 'B': ");
+B = Convert.ToDouble(Console.ReadLine());
+
+result = A * B;
+Console.WriteLine("\nSide A has a value of: " + A + "\nSide B has a value of: " + B + "\nThe result is: " + result);
