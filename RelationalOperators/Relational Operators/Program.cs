5cb78b37d700f3cccea12b8100c81b10f506f255
Damian <sasakihaise1021@gmail.com>
2023-12-12 23:14:57 -0500

    Exercise with relational operators

---
+++ RelationalOperators/Relational Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿var (value1, value2, value3) = (10, 10, 2);
+
+Console.WriteLine("The values are: " + value1 + " " + value2 + " " + value3);
+
+bool result1 = value1 == value2;
+Console.WriteLine("The result of comparasion value1 == value2 is: " + result1);
+
+bool result2 = value1 != value2;
+Console.WriteLine("The result of comparasion value1 != value2 is: " + result2);
+
+bool result3 = value1 > value2;
+Console.WriteLine("The result of comparasion value1 > value2 is: " + result3);
+
+bool result4 = value1 < value2;
+Console.WriteLine("The result of comparasion value1 < value2 is: " + result4);
+
+bool result5 = value1 >= value2;
+Console.WriteLine("The result of comparasion value1 >= value2 is: " + result5);
+
+bool result6 = value1 <= value2;
+Console.WriteLine("The result of comparasion value1 <= value2 is: " + result6);
