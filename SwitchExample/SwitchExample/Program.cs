8e167f3db6358f0e5af989bd3b01ab54fcbb8e86
Damian <sasakihaise1021@gmail.com>
2023-12-14 00:16:09 -0500

    Exercise with switch, a very simple example

---
+++ SwitchExample/SwitchExample/Program.cs
@@ -0,0 +1,34 @@
+﻿int totalPlayer = 15;
+int totalDealer = 15;
+string message = "";
+string SwitchControl = "pene";
+
+switch (SwitchControl)
+{
+    case "menu":
+        Console.WriteLine("Welcome to E L  C A S I N O");
+        Console.WriteLine("Write '21' to start the game Blackjack");
+        SwitchControl = Console.ReadLine();
+        break;
+
+    case "21":
+        if (totalPlayer > totalDealer && totalPlayer < 22)
+        {
+            message = "You win, congratulations!";
+        }
+        else if (totalPlayer <= totalDealer || totalPlayer > 21)
+        {
+            message = "You lose, try again";
+        }
+        else
+        {
+            message = "Invalid condition, try and play again";
+        }
+
+        Console.WriteLine(message);
+        break;
+
+    default:
+        Console.WriteLine("The value is not valid");
+        break;
+}
