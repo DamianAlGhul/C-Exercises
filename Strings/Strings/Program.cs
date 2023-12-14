64b18305dad0e7a90679ea99856a3ec9ff13d999
Damian <sasakihaise1021@gmail.com>
2023-12-13 23:02:58 -0500

    Exercise with strings, how to use it and how to put strings in other strings with $

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Jhon Escobar ID: ");
+
+int height = 171;
+int age = 20;
+string name = "Jhon Escobar";
+string Information = "He's born in San Antonio, is a student of engineery systems";
+var hobby = "Gym rat";
+
+string IdentificationTarget = $"The information of {name} is this: \n" +
+    $"Age is {age} years old\n" +
+    $"Her height is {height} centimeters\n" +
+    $"Some relevant information is {Information}\n" +
+    $"And her hobby is be a {hobby}";
+
+
+Console.WriteLine(IdentificationTarget);
