55b4a7f20b470d712189e904b3fa8e72c2fc28aa
Damian <sasakihaise1021@gmail.com>
2023-12-13 23:53:45 -0500

    Exercise with if, else if and else, the structure and use

---
+++ ifexample/ifexample/Program.cs
@@ -0,0 +1,17 @@
+﻿int totalPlayer = 15;
+int totalDealer = 15;
+string message = "";
+
+if (totalPlayer > totalDealer && totalPlayer < 22)
+{
+    message = "You win, congratulations!";
+}
+else if(totalPlayer <= totalDealer || totalPlayer > 21){
+    message = "You lose, try again";
+}
+else
+{
+    message = "Invalid condition, try and play again";
+}
+
+Console.WriteLine(message);
